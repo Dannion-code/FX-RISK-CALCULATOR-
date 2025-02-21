a0a91c00d8ee458b0a01e6b65fec2f861ded4ad2
Dannion-code <77624128+Dannion-code@users.noreply.github.com>
2025-02-21 21:15:53 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,20 @@
 const CACHE_NAME = "fx-calculator-v1";
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/script.js",
-  "/lot-size.js",
-  "/pips-points.js",
-  "/profit-loss.js",
-  "/manifest.json",
-  "/service-worker.js",
-  "/assets/icon-192.png",
-  "/assets/icon-512.png"
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./lot-size.js",
+  "./pips-points.js",
+  "./profit-loss.js",
+  "./manifest.json",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
@@ -31,10 +28,10 @@ self.addEventListener("fetch", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+      )
+    )
   );
 });
